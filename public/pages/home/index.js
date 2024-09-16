d0a5187fe6684e4b51da3a335b178195327c2ae9
hookor <gerrard1219@gmail.com>
2024-09-16 18:54:08 +0900

    Feat: initial entry point structure

--- public/pages/home/index.js
+++ public/pages/home/index.js
@@ -1,20 +1,88 @@
-import { useHighlight } from '../../hooks/useHighlight';
-import styles from './style.module.css';
-import { useState } from 'preact/hooks';
+const { default: jsonObject } = await import('../../posts.json', {
+  assert: {
+    type: 'json',
+  },
+});
 
 export default function Home() {
-  const [count, setCount] = useState(0);
-  const snippet = useHighlight(
-    'typescript',
-    `function createPair<S, T>(v1: S, v2: T): [S, T] {
-		return [v1, v2];
-	}
-console.log(createPair<string, number>('hello', 42)); // ['hello', 42]`
-  );
+  const recentPosts = Object.entries(jsonObject)
+    .flatMap(([_, items]) => {
+      return items.map((item) => {
+        const [category, post] = Object.entries(item)[0];
+        return [category, post[0], post[1]];
+      });
+    })
+    .sort((a, b) => new Date(b[2]).getTime() - new Date(a[2]).getTime())
+    .slice(0, 5);
 
   return (
     <>
-      <p>{snippet}</p>
+      <h2>Hi I'm JoongHoo Ahn</h2>
+      <blockquote cite="https://motherfuckingwebsite.com">
+        <strong>"You. Are. Over-designing"</strong>
+      </blockquote>
+      <p>
+        This blog is
+        <ol>
+          <li>
+            greatly inspired by
+            <a target={'_blank'} href="https://motherfuckingwebsite.com">
+              {' '}
+              motherfuckingwebsite.com
+            </a>
+          </li>
+          <li>
+            built with
+            <a target={'_blank'} href="https://preactjs.com/">
+              {' '}
+              Preact
+            </a>
+            ,
+            <a target={'_blank'} href="https://highlightjs.org/">
+              {' '}
+              highlight.js
+            </a>
+          </li>
+          <li>
+            created by
+            <a target={'_blank'} href={'https://github.com/hookor'}>
+              {' '}
+              hookor
+            </a>
+          </li>
+        </ol>
+      </p>
+
+      <p>
+        <em>Unlike</em> the source of inspiration above, I'm not cynical :)
+        <br />
+        Feel free to follow me on Github, or contact via{' '}
+        <a target={'_blank'} href="mailto:gerrard1219@gmail.com">
+          E-mail
+        </a>
+        .
+      </p>
+
+      <h2>Recent Posts</h2>
+      {jsonObject && (
+        <ul>
+          {recentPosts.map((post) => {
+            const [category, title, date] = post;
+            return (
+              <li>
+                <span style={'font-size: 14px; padding-right: 14px;'}>
+                  {new Date(date).toLocaleDateString('en-us', {
+                    year: 'numeric',
+                    month: 'short',
+                    day: 'numeric',
+                  })}
+                </span>
+                <a href={`/posts/${category}/${title}`}>{title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </>
   );
 }
