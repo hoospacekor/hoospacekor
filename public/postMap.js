b4ab32c5d25c42fcc2da9473c0e52ab7501d5103
hookor <gerrard1219@gmail.com>
2024-11-08 03:55:04 +0900

    Feat: new post

--- public/postMap.js
+++ public/postMap.js
@@ -3,4 +3,5 @@ export const postModules = {
   '/posts/nodejs/how-to-make-a-list-of-files-from-directories': () => import('./posts/nodejs/how-to-make-a-list-of-files-from-directories.js'),
   '/posts/personal/colorless-odourless': () => import('./posts/personal/colorless-odourless.js'),
   '/posts/personal/hello': () => import('./posts/personal/hello.js'),
+  '/posts/personal/snob': () => import('./posts/personal/snob.js'),
 };
